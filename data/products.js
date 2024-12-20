44289776b02369792b9b7925f89570df0bba7a21
leminhhuywork <leminhhuy.work@gmail.com>
2024-12-20 12:07:46 +0700

    Add product.js file

---
+++ data/products.js
@@ -0,0 +1,110 @@
+const products = [
+    {
+        image: 'images/products/51ld6RR8IrL._AC_SY200_.jpg',
+        name: 'Product Name',
+        rating: {
+            stars: 4.5,
+            counter: 87
+        },
+        priceCents: 1090
+    },
+    {
+        image: 'images/products/51zaCVh6SIL._AC_SY200_.jpg',
+        name: 'Product Name',
+        rating: {
+            stars: 4.5,
+            counter: 87
+        },
+        priceCents: 1090
+    },
+    {
+        image: 'images/products/Fuji_Dash_SmartWatch_1X._SY304_CB639922137_.jpg',
+        name: 'Product Name',
+        rating: {
+            stars: 4.5,
+            counter: 87
+        },
+        priceCents: 1090
+    },
+    {
+        image: 'images/products/71jfsEP8G4L._AC_SY200_.jpg',
+        name: 'Product Name',
+        rating: {
+            stars: 4.5,
+            counter: 87
+        },
+        priceCents: 1090
+    },
+    {
+        image: 'images/products/Fuji_Gaming_store_Dashboard_card_1x_EN._SY304_CB564799420_.jpg',
+        name: 'Product Name',
+        rating: {
+            stars: 4.5,
+            counter: 87
+        },
+        priceCents: 1090
+    },
+    {
+        image: 'images/products/LSS23_SPRING_DT_CAT_CARD_1_x1._SY116_CB595261253_.jpg',
+        name: 'Product Name',
+        rating: {
+            stars: 4.5,
+            counter: 87
+        },
+        priceCents: 1090
+    },
+    {
+        image: 'images/products/Tablet_1x._SY116_CB566164844_.jpg',
+        name: 'Product Name',
+        rating: {
+            stars: 4.5,
+            counter: 87
+        },
+        priceCents: 1090
+    },
+    {
+        image: 'images/products/51ld6RR8IrL._AC_SY200_.jpg',
+        name: 'Product Name',
+        rating: {
+            stars: 4.5,
+            counter: 87
+        },
+        priceCents: 1090
+    },
+    {
+        image: 'images/products/51ld6RR8IrL._AC_SY200_.jpg',
+        name: 'Product Name',
+        rating: {
+            stars: 4.5,
+            counter: 87
+        },
+        priceCents: 1090
+    },
+    {
+        image: 'images/products/51ld6RR8IrL._AC_SY200_.jpg',
+        name: 'Product Name',
+        rating: {
+            stars: 4.5,
+            counter: 87
+        },
+        priceCents: 1090
+    },
+    {
+        image: 'images/products/51ld6RR8IrL._AC_SY200_.jpg',
+        name: 'Product Name',
+        rating: {
+            stars: 4.5,
+            counter: 87
+        },
+        priceCents: 1090
+    },
+    {
+        image: 'images/products/51ld6RR8IrL._AC_SY200_.jpg',
+        name: 'Product Name',
+        rating: {
+            stars: 4.5,
+            counter: 87
+        },
+        priceCents: 1090
+    },
+]
